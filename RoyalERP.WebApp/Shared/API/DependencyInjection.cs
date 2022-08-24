0d5ba7b2c88db72608e6c75376105acd12ad0335
ZachLondono <zachary.londono5@gmail.com>
2022-08-23 23:30:25 -0400

    Added new data to dependency injection

--- RoyalERP.WebApp/Shared/API/DependencyInjection.cs
+++ RoyalERP.WebApp/Shared/API/DependencyInjection.cs
@@ -3,6 +3,8 @@ using Refit;
 using RoyalERP.WebApp.Shared.API.WorkOrders;
 using RoyalERP.WebApp.Shared.API.Companies;
 using RoyalERP.WebApp.Shared.API.Events;
+using RoyalERP.WebApp.Shared.API.Products;
+using RoyalERP.WebApp.Shared.API.ProductClasses;
 
 namespace RoyalERP.WebApp.Shared.API;
 
@@ -15,7 +17,9 @@ public static class DependencyInjection {
         return services.AddScoped<ICompanyAPI>(s => new ExampleCompanyData())
                         .AddScoped<IOrderAPI>(s => new ExampleOrderData())
                         .AddScoped<IWorkOrderAPI>(s => new ExampleWorkOrderData())
-                        .AddScoped<IEventAPI>(s => new ExampleEventData());
+                        .AddScoped<IEventAPI>(s => new ExampleEventData())
+                        .AddScoped<IProductAPI>(s => new ExampleProductData())
+                        .AddScoped<IProductClassAPI>(s => new ExampleProductClassData());
 
         /*return services.AddScoped(s => RestService.For<IOrderAPI>(host))
                         .AddScoped(s => RestService.For<ICompanyAPI>(host))
